ffd4b3398fddb4fb28c652d4e615e6a2bf5cfbc2
Venilton FalvoJr <falvojr@gmail.com>
2016-09-15 21:10:07 -0300

    Model refactoring.

--- backend/src/main/java/br/com/entelgy/model/VoteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.entelgy.model;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "votes", path = "votes")
-public interface VoteRepository extends MongoRepository<Vote, String> {
-
-}
